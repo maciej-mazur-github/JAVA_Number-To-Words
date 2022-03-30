a243f1f6afdfa28c4ea7035382214fc545ab1f71
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-03-30 13:10:40 +0200

    Add getDigitCount(int) and numberToWords(int) methods

--- src/NumberToWords.java
+++ src/NumberToWords.java
@@ -4,6 +4,10 @@ public class NumberToWords {
             return -1;
         }
 
+        if (number == 0) {
+            return 1;
+        }
+
         int digitCounter = 0;
 
         while(number > 0) {
@@ -26,6 +30,75 @@ public class NumberToWords {
         return reversedNumber;
     }
 
+    public static void numberToWords(int number) {
+        if(number < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        if(number == 0) {
+            System.out.println("Zero");
+            return;
+        }
+
+        int reversedNumber = reverse(number);
+
+        int originalDigitCount = getDigitCount(number);
+        int reversedDigitCount = getDigitCount(reversedNumber);
+        int leadingZerosCount = originalDigitCount - reversedDigitCount;
+
+        String wordToPrint;
+        int currentDigit;
+
+        for(int i = 0; i < reversedDigitCount; i++) {
+            currentDigit = reversedNumber % 10;
+
+            switch (currentDigit) {
+                case 0:
+                    System.out.print("Zero ");
+                    break;
+                case 1:
+                    System.out.print("One ");
+                    break;
+                case 2:
+                    System.out.print("Two ");
+                    break;
+                case 3:
+                    System.out.print("Three ");
+                    break;
+                case 4:
+                    System.out.print("Four ");
+                    break;
+                case 5:
+                    System.out.print("Five ");
+                    break;
+                case 6:
+                    System.out.print("Six ");
+                    break;
+                case 7:
+                    System.out.print("Seven ");
+                    break;
+                case 8:
+                    System.out.print("Eight ");
+                    break;
+                case 9:
+                    System.out.print("Nine ");
+                    break;
+            }
+
+            reversedNumber /= 10;
+        }
+
+        if(leadingZerosCount > 0) {
+            for(int i = 0; i < leadingZerosCount; i++) {
+                System.out.print("Zero ");
+            }
+        }
+
+        System.out.println();
+
+    }
+
 
     public static void main(String[] args) {
         System.out.println(getDigitCount(5));
@@ -37,5 +110,11 @@ public class NumberToWords {
         System.out.println(reverse(212));
         System.out.println(reverse(432));
         System.out.println(reverse(-234));
+        System.out.println("***************************");
+        numberToWords(12345);
+        numberToWords(5);
+        numberToWords(-10);
+        numberToWords(22);
+        numberToWords(23746);
     }
 }
